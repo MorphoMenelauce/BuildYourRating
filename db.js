5625221c4eb3b7f363092591be3054ffa098ad71
sirius <Morpho>
2020-12-02 10:56:33 +0100

    etape 1 done

--- db.js
+++ db.js
@@ -6,8 +6,8 @@ const PORT = argv.port || 3000; // Utilisation du port en paramètre ou par defa
 
 const Server = require('socket.io');
 const io = new Server(PORT, { // Création du serveur
-  path: '/byr',
-  serveClient: false,
+    path: '/byr',
+    serveClient: false,
 });
 
 console.info(`Serveur lancé sur le port ${PORT}.`);
@@ -15,26 +15,26 @@ console.info(`Serveur lancé sur le port ${PORT}.`);
 const db = Object.create(null); // Création de la DB
 
 io.on('connect', (socket) => { // Pour chaque nouvlle connexion
-  console.info('Nouvelle connexion');
+    console.info('Nouvelle connexion');
 
-  socket.on('get', function(field, callback){
-    console.info(`get ${field}: ${db[field]}`);
-    callback(db[field]);
-  });
+    socket.on('get', function (field, callback) {
+        console.info(`get ${field}: ${db[field]}`);
+        callback(db[field]);
+    });
 
-  socket.on('set', function(field, value, callback){
-    if (field in db) {
-      console.info(`set error : Field ${field} exists.`);
-      callback(false);
-    } else {
-      console.info(`set ${field} : ${value}`);
-      db[field] = value;
-      callback(true);
-    }
-  });
+    socket.on('set', function (field, value, callback) {
+        if (field in db) {
+            console.info(`set error : Field ${field} exists.`);
+            callback(false);
+        } else {
+            console.info(`set ${field} : ${value}`);
+            db[field] = value;
+            callback(true);
+        }
+    });
 
-  socket.on('keys', function(callback){
-    console.info(`keys`);
-    callback(Object.keys(db));
-  });
+    socket.on('keys', function (callback) {
+        console.info(`keys`);
+        callback(Object.keys(db));
+    });
 });
